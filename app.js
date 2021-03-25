6c8f0dfcf7308f2ef9826a6c7cc5f7befbf2f567
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-03-25 12:37:46 +0700

    simple string chat

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', socket => {
+    // Welcome current user
+    socket.emit('message', 'Welcome to MP Chat!')
+
+    // Broadcast when a user connects
+    socket.broadcast.emit('message', 'A user has join the chat');
+
+    // Broadcast when a user disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    });
+
+    // Listen for chatMessage 
+    socket.on('chatMessage', (msg) => {
+        console.log(msg);
+        io.emit('message', msg)
+    })
+})
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- public/js/main.js
+++ public/js/main.js
@@ -23,3 +23,58 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   
   });
+
+  const chatForm = document.getElementById('chat-form');
+  const socket = io();
+  const chatMessages = document.querySelector('.is-scrollable');
+
+  // Message from server
+  socket.on('message', message => {
+    console.log(message);
+    outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  })
+
+  // Message submit
+  chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Get message text
+    const msg = e.target.elements.msg.value;
+
+    // Emit message to server
+    socket.emit('chatMessage', msg);
+
+     // Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+  })
+
+  // Output message to DOM
+  function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('box');
+    div.classList.add('is-primary');
+    div.innerHTML = `<article class="media">
+    <figure class="media-left">
+      <p class="image is-64x64">
+        <img
+          src="https://bulma.io/images/placeholders/128x128.png"
+        />
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content   ">
+          <strong>John Smith</strong>
+          <small>@johnsmith</small>
+          <small>31m</small>
+          <br />
+          ${message}
+        </p>
+      </div>
+    </div>
+  </article>`;
+  document.querySelector('.is-scrollable').appendChild(div);
+  }
