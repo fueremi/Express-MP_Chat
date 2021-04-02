bc3fb587568628ec56335d1be7733e8ed3747596
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-04-03 04:20:51 +0700

    Fixing timezone to local timezoen (which local timezone deployment server, US/Virginia) to Indonesia/Jakarta timezone

--- utils/message.js
+++ utils/message.js
@@ -2,8 +2,11 @@ const moment = require('moment');
 
 function formatMessage(username, text){
     return {
+        // Username of message came from
         username,
+        // Message that user send 
         text,
+        // Formatted timezone to Jakarta/Indonesia timezone
         time: moment().zone('+07:00').format('h:mm a')
     }
 
